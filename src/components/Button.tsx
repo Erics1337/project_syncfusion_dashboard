8297bf7ff0a084cf77a48fd03d3ae5c5d86a16c1
Eric <erics1337@gmail.com>
2022-05-25 23:05:33 -0600

    Additions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,37 @@
 import React from 'react'
 
+import { useStateContext } from '../contexts/ContextProvider'
+
 type Props = {
-	bgColor: string
-	color: string
-	size: string
-	text: string
-	borderRadius: string
+	icon?: any
+	bgHoverColor?: string
+	bgColor?: string
+	color?: string
+	size?: string
+	width?: any
+	text?: string
+	borderRadius?: string
 }
 
-const Button = ({ bgColor, color, size, text, borderRadius }: Props) => {
+const Button = ({
+	icon,
+	bgColor,
+	color,
+	bgHoverColor,
+	size,
+	text,
+	borderRadius,
+	width,
+}: Props) => {
+	const { setIsClicked, initialState } = useStateContext()
+
 	return (
 		<button
 			type='button'
+			onClick={() => setIsClicked(initialState)}
 			style={{ backgroundColor: bgColor, color, borderRadius }}
-			className={`text-[${size}] p-3 hover:drop-shadow-xl`}>
-			{text}
+			className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}>
+			{icon} {text}
 		</button>
 	)
 }
